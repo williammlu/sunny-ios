aded7975c50b4d2762263201fa7fc52708ffeb7a
William Lu <lu.william.m@gmail.com>
2025-03-01 15:11:06 -0800

    Initial Commit

---
+++ sunny/sunnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunnyApp.swift
+//  sunny
+//
+//  Created by William Lu on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct sunnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sunnyTests/sunnyTests.swift
@@ -0,0 +1,17 @@
+//
+//  sunnyTests.swift
+//  sunnyTests
+//
+//  Created by William Lu on 3/1/25.
+//
+
+import Testing
+@testable import sunny
+
+struct sunnyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
